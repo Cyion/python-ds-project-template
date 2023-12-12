5b7a8f19a89d47f708e85ef3c593bdfe032d75e6
Pascal Reichert <pascal.reichert1996@gmail.com>
2023-12-12 11:31:42 +0100

    implemented creation of __init__.py in subfolders of src

--- src/template.py
+++ src/template.py
@@ -8,24 +8,27 @@ _dir_list = [
     "models",
     "notebooks",
     "reports",
-    "src/data",
-    "src/model",
-    "src/visualization",
+    "src/projectname/data",
+    "src/projectname/model",
+    "src/projectname/visualization",
 ]
 
 _file_list = [
     "README.md",
     "requirements.txt",
-    "src/__init__.py",
+    "src/projectname/__init__.py",
+    "src/projectname/data/__init__.py",
+    "src/projectname/model/__init__.py",
+    "src/projectname/visualization/__init__.py",
 ]
 
 def _create_project_structure(args):
     root = f"{args.projectname}/"
     for dir in _dir_list:
-        os.makedirs(root + dir, exist_ok=True)
+        os.makedirs(root + dir.replace("projectname", args.projectname), exist_ok=True)
 
     for file in _file_list:
-        filepath = root + file
+        filepath = root + file.replace("projectname", args.projectname)
         if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
             open(filepath,'a')
 
